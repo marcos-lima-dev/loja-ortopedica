7bb2b7853e583ff4a7251dec938cfdb06266ed22
Marcos de Sousa Lima <sousa.lima@gmail.com>
2025-10-31 07:42:28 -0300

    feat: inicializa projeto com Tailwind v4 + Vite + React TS

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="min-h-screen bg-purple-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-purple-800">
+        Será que está funcionando agora Kakaroto  🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
